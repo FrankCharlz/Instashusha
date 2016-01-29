f03147b261604a27961178633f2a5bc3db4d0c89
FrankCharlz <mjcharlz@gmail.com>
2016-01-29 20:41:53 +0300

    Time to sleep

--- app/src/main/java/com/mj/instashusha/fragments/InstructionFragment.java
+++ app/src/main/java/com/mj/instashusha/fragments/InstructionFragment.java
@@ -1,13 +1,18 @@
 package com.mj.instashusha.fragments;
 
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -16,6 +21,9 @@ import com.mj.instashusha.R;
 import com.mj.instashusha.activities.DownloadedActivity;
 import com.mj.instashusha.activities.IntroActivity;
 import com.mj.instashusha.utils.DopeTextView;
+import com.squareup.okhttp.internal.Util;
+
+import java.util.logging.Handler;
 
 /**
  * Created by Frank on 12/19/2015.
@@ -23,6 +31,12 @@ import com.mj.instashusha.utils.DopeTextView;
 public class InstructionFragment extends Fragment {
 
     private Context context;
+    private DopeTextView instructionTv;
+    private int counter;
+    private int total;
+    private String[] messages;
+    private Animation swap, swapIn;
+    private Animation swap0;
 
     @Nullable
     @Override
@@ -46,8 +60,106 @@ public class InstructionFragment extends Fragment {
         final DopeTextView btnOpenInsta = (DopeTextView) root.findViewById(R.id.btn_fungua_insta);
         btnOpenInsta.setOnClickListener(new ButtonClicks());
 
+        messages = context.getResources().getStringArray(R.array.ins_messages);
+        counter = 0;
+        total = messages.length;
+
+
+        instructionTv = (DopeTextView) root.findViewById(R.id.tv_instructions);
+        instructionTv.setOnTouchListener(null);
+
+        swap = AnimationUtils.loadAnimation(context, R.anim.in_out);
+
         return root;
+
     }
+
+    public static class FuckingFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fucking_fragment, container, false);
+        }
+    }
+
+    /***
+     *  class Touch implements View.OnTouchListener {
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+    float x0 = 0, x1 = 0;
+    switch (event.getAction()) {
+    case MotionEvent.ACTION_DOWN:
+    x0 = event.getX();
+    break;
+    case MotionEvent.ACTION_UP:
+    x1 = event.getX();
+    break;
+    default:  break;
+    }
+
+    if (x1 - x0 > 60) swapRight();
+    else if (x1 - x0 > 60) swapLeft();
+
+    return true;
+    }
+    }
+
+     /*
+     private void swapLeft() {
+     InstagramApp.log("Swapping left");
+     TranslateAnimation tr = new TranslateAnimation(
+     0,
+     instructionTv.getWidth(),
+     0,
+     0);
+     tr.setDuration(1000);
+     tr.setAnimationListener(new MyAnimationListener());
+     instructionTv.startAnimation(swap);
+
+     }
+
+     private void swapRight() {
+     InstagramApp.log("Swapping right");
+     TranslateAnimation tr = new TranslateAnimation(
+     0,
+     -1*instructionTv.getWidth(),
+     0,
+     0);
+     tr.setDuration(1000);
+     tr.setAnimationListener(new MyAnimationListener());
+     instructionTv.startAnimation(swap);
+
+     }
+
+
+     class MyAnimationListener implements Animation.AnimationListener {
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+    InstagramApp.log("Animation starting : counter : "+counter);
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+    InstagramApp.log("Animation ending : counter : " + counter);
+    InstagramApp.log("Anim : "+animation.toString());
+    counter++;
+    counter = counter % total;
+    instructionTv.setText(messages[counter]);
+    }
+
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+    }
+
+     */
+
 
     class ButtonClicks implements View.OnClickListener {
 
